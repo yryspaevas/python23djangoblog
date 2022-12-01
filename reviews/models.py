8383a554a29e227f015e8e2a691c9730d500bbe5
eles <yryspaeva22@gmail.com>
2022-12-01 10:30:57 +0600

    create comment model

---
+++ reviews/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth import get_user_model
+# from django.db import models
+from django.db import models
+from main.models import Post
+# Create your models here.
+User = get_user_model()
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, related_name='comments', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
